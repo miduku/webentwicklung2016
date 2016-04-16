8fa3709451a5d0e9fdf5b217b2277fa10d114bd2
Dustin K <miduku11@gmail.com>
2016-04-16 17:07:49 +0200

    update 160416

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,38 +7,75 @@
     $('body').prepend('<div class="hamburgerBgr"></div>');
 
     // create hamburger menu .navToggle
-    $('.nav-main').prepend('<button class="navToggle"><span></span></button>');
+    $('.nav-main').prepend('<button class="navToggle"><span></span></button><span class="nav-title"></span>');
+
+    // clone the text from the active menü to the nav-title
+    $('.nav-title').append($('.nav-main li.active').text());
 
     // toggle
     $('.nav-main > ul').hide();
-    $('body').on('click', '.navToggle', function(event) {
-      event.preventDefault();
+    $('body')
+      .on('click', '.navToggle', function(event) {
+        event.preventDefault();
 
-      if ($('.nav-main').hasClass('active')) {
+        if ($('.nav-main').hasClass('active')) {
+          $('.nav-main')
+            .removeClass('active')
+            .children('ul')
+            .slideUp('fast')
+            .siblings('.nav-title')
+            // .addClass('visible')
+            .show('fast');
+
+            if ($(window).scrollTop() > $('.header').outerHeight()) {
+              $('.nav-title').addClass('visible');
+            }
+
+          $('.hamburgerBgr').removeClass('active');
+        }
+        else {
+          $('.nav-main')
+            .addClass('active')
+            .children('ul')
+            .slideDown('fast')
+            .siblings('.nav-title')
+            .removeClass('visible')
+            .hide('fast');
+
+          $('.hamburgerBgr').addClass('active');
+        }
+      })
+      .on('click', '.hamburgerBgr', function() {
+        if ($('.nav-main').hasClass('active')) {
+          $(this).removeClass('active');
+
         $('.nav-main')
           .removeClass('active')
           .children('ul')
           .hide('fast');
+        }
+      });
 
-        $('.hamburgerBgr').removeClass('active');
-      }
-      else {
-        $('.nav-main')
-          .addClass('active')
-          .children('ul')
+    // show nav-title after scroll
+    var isVisible = false;
+    $(window).on('scroll resize', function() {
+      var shouldBeVisible = $(window).scrollTop() > $('.header').outerHeight();
+
+      if (!$('.nav-main').hasClass('active') && shouldBeVisible && !isVisible) {
+        isVisible = true;
+        $('.nav-title')
+          .addClass('visible')
           .show('fast');
-
-        $('.hamburgerBgr').addClass('active');
       }
-    }).on('click', '.hamburgerBgr', function(event) {
-      if ($('.nav-main').hasClass('active')) {
-        $(this).removeClass('active');
-
-      $('.nav-main')
-        .removeClass('active')
-        .children('ul')
-        .hide('fast');
+      else if (!$('.nav-main').hasClass('active') && !shouldBeVisible && isVisible) {
+        isVisible = false;
+        $('.nav-title')
+          .hide('fast')
+          .removeClass('visible');
       }
+
+
+      console.log(isVisible);
     });
   });
 
